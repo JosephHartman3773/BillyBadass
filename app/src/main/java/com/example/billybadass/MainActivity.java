45f26faf5eb6f53e42a65c51fb23177eb522c686
Admin <admin@Admins-MacBook-Pro.local>
2019-09-04 14:34:31 -0400

    Initial commit

---
+++ app/src/main/java/com/example/billybadass/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.billybadass;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public String toString() {
+        return "MainActivity{}";
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        System.out.println("starting app");
+    }
+}
